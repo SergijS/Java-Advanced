5e16767dbb1aa6c909399fab2eba8f2be4468a0f
SergijS <107573014+SergijS@users.noreply.github.com>
2023-03-20 00:49:38 +0200

    Delete HW#3 JDBC_Project_last/src/main/java/org/example directory

--- HW#3 JDBC_Project_last/src/main/java/org/example/Connections.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Connections {
-    public final static String url = "jdbc:mysql://localhost:3306/mysql";
-    public final static String user = "root";
-    public final static String password = "!1234A";
-}
