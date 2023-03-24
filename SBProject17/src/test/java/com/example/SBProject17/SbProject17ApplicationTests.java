1aa3999db62dc36e012d6a764ce793d3abb496c1
SergijS <107573014+SergijS@users.noreply.github.com>
2023-03-24 19:23:45 +0200

    Add files via upload

---
+++ SBProject17/src/test/java/com/example/SBProject17/SbProject17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBProject17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbProject17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
